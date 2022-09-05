473265744e97440bd13302b48bf71f2f96bd6bb0
7sering <codenow8@gmail.com>
2022-09-05 12:10:49 +0545

    Landing Page Complete

--- base/urls.py
+++ base/urls.py
@@ -1,10 +1,12 @@
 
 from django.contrib import admin
 from django.urls import path, include
-from django.urls import path
 from landing import urls
+
+from django.conf.urls.static import static #import setting
+from django.conf import settings # Import Settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('landing.urls')),
 
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) #media file configuration in url
