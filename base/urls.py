1cfda1944bd8d79bc9628bb48076c3450aa7a3ee
7sering <codenow8@gmail.com>
2022-09-06 20:17:44 +0545

    User Auth Setup

--- base/urls.py
+++ base/urls.py
@@ -2,11 +2,16 @@
 from django.contrib import admin
 from django.urls import path, include
 from landing import urls
+from authenticate_user import urls
+
 
 from django.conf.urls.static import static #import setting
 from django.conf import settings # Import Settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('landing.urls')),
+    path('', include('authenticate_user.urls'))
+
+
 
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) #media file configuration in url
