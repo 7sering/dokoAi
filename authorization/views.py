a34f02d4160658f6bfb9bc563281b918de42570c
coder <tsering@Tsering>
2022-09-07 22:22:58 +0545

    Register UI Buil Up

--- authorization/views.py
+++ authorization/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 
 def login(request):
-    return render(request, 'user/login.html')
+    return render(request, 'authorization/login.html')
 
 def register(request):
-    return render(request, 'user/register.html')    
+    return render(request, 'authorization/register.html')    
