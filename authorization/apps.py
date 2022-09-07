c0eb3488fe9d7b105516832e9e05a05da8f610dc
coder <tsering@Tsering>
2022-09-07 21:36:47 +0545

    Authorization Bug Fixed

--- authorization/apps.py
+++ authorization/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class AuthenticateUserConfig(AppConfig):
+class AuthorizationConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'authenticate_user'
+    name = 'authorization'
