1183df678290ef8f7cb88025a77b44e169adae66
coder <tsering@Tsering>
2022-09-08 12:42:48 +0545

    Small Fix On Auth UI

--- authorization/views.py
+++ authorization/views.py
@@ -1,9 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
+
 
 def login(request):
     return render(request, 'authorization/login.html')
 
+
 def register(request):
-    return render(request, 'authorization/register.html')    
+    if request.method == 'POST':
+        email = request.POST['email']
+        password = request.POST['password']
+        confirm_password = request.POST['confirm_password']
+
+        print('Username Submitted: {} ' .format(email, password))
+        return redirect('register')
+    return render(request, 'authorization/register.html')
