0281159ded8e83fb27950d23417fea42ee45fa07
coder <tsering@Tsering>
2022-09-13 15:08:28 +0545

    Profile UI setting & base fixed

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -7,6 +7,11 @@ urlpatterns = [
     path('login', views.login, name='login'),
     path('register',views.register, name='register'),
     path('logout',views.logout, name='logout'),
+    path('profile', views.profile, name='profile')
+
+
+
+
     # path('dashboard', views.dashboard, name="dashboard")
 
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -77,3 +77,7 @@ def logout(request):
 @login_required
 def dashboard(request):
     return render(request, 'dashboard/dashboard.html')
+
+@login_required
+def profile(request):
+    return render(request, 'dashboard/profile.html')
